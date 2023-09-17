e2ff208a566485dedb457d1058354db06d1a53f4
pranti <prantibanda@gmail.com>
2023-09-17 14:22:19 +0530

    Modified Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      <div className="flex  shadow-sm items-center p-3">
+      <div className="flex border-b-2 border-slate-100 items-center p-3">
 				<div className="font-extrabold text-lg text-blue-900">EDUAPP</div>
 				<div className="flex ml-auto text-orange-300 font-medium space-x-4 pr-4 ">
-					<div className="p-1 hover:bg-orange-100 hover:border-orange-200 text-sm rounded-md">HOME</div>
-					<div className="p-1 hover:bg-orange-100 hover:border-orange-200 text-sm rounded-md">ROADMAP</div>
-					<div className="p-1 hover:bg-orange-100 hover:border-orange-200 text-sm rounded-md">QUIZ</div>
-					<div className="p-1 hover:bg-orange-100 hover:border-orange-200 text-sm rounded-md">SIGN IN</div>
+					<div className="py-1 px-2 hover:bg-orange-50 text-sm rounded-md">HOME</div>
+					<div className="py-1 px-2 hover:bg-orange-50 text-sm rounded-md">ROADMAP</div>
+					<div className="py-1 px-2 hover:bg-orange-50 text-sm rounded-md">QUIZ</div>
+					<div className="py-1 px-2 hover:bg-orange-50 text-sm rounded-md">SIGN IN</div>
 				</div>
 				<div className="material-symbols-outlined bg-purple-200 text-purple-400 rounded-full p-1 hover:text-white hover:bg-purple-400">person</div>
 			</div>
