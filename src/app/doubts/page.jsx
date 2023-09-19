2818888a8ca55f1a654d50810afc834228358091
code-with-santanu <santanupal003@gmail.com>
2023-09-20 00:10:54 +0530

    updated

--- src/app/doubts/page.jsx
+++ src/app/doubts/page.jsx
@@ -5,6 +5,12 @@ import React, { useState } from "react";
 export default function doubt() {
   const [query, setQuery] = useState("");
   const [ans, setAns] = useState("");
+  const [arr, setArr] = useState([
+    {
+      q: "Hello, Here is your personal assistant",
+      a: "How can I help you?",
+    },
+  ]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,7 +19,7 @@ export default function doubt() {
 
     const axios = require("axios");
 
-    const apiKey = "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";
+    const apiKey = "";
     const client = axios.create({
       headers: { Authorization: "Bearer " + apiKey },
     });
@@ -21,13 +27,15 @@ export default function doubt() {
     const params = {
       prompt: query,
       max_tokens: 200,
+      temperature: 0.07,
     };
 
     client
       .post("https://api.openai.com/v1/engines/davinci/completions", params)
       .then((result) => {
         var data = result.data.choices[0].text;
-        console.log(params.prompt + "\n" + result.data.choices[0].text);
+        console.log(data);
+        console.log(arr);
         setAns(data);
       })
       .catch((err) => {
@@ -35,10 +43,31 @@ export default function doubt() {
       });
   };
 
+  const addResponse = (query, ans) => {
+    let newData = [
+      {
+        q: query,
+        a: ans,
+      },
+    ];
+    setArr([...arr, newData]);
+    console.log("addResponse done");
+  };
+
   return (
     <div className="container">
       <div>
+        <p>{query}</p>
+        <br />
         <p>{ans}</p>
+
+        {arr.map((data, idx) => (
+          <li className="list-none" key={idx}>
+            <p>{data.q}</p>
+            <br />
+            <p>{data.a}</p>
+          </li>
+        ))}
       </div>
       <div className="doubts">
         <input
@@ -46,7 +75,13 @@ export default function doubt() {
           value={query}
           onChange={({ target }) => setQuery(target?.value)}
         />
-        <button type="submit" onClick={handleSubmit}>
+        <button
+          type="submit"
+          onClick={() => {
+            handleSubmit(event);
+            addResponse(query, ans);
+          }}
+        >
           Enter
         </button>
       </div>
