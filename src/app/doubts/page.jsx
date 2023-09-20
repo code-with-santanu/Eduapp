b8002646b031089c670b3e1f69b2e3c5e3e57a9c
code-with-santanu <santanupal003@gmail.com>
2023-09-20 21:10:20 +0530

    updated and working properly

--- src/app/doubts/page.jsx
+++ src/app/doubts/page.jsx
@@ -3,71 +3,75 @@
 import React, { useState } from "react";
 
 export default function doubt() {
-  const [query, setQuery] = useState("");
-  const [ans, setAns] = useState("");
-  const [arr, setArr] = useState([
+  const a = [
     {
       q: "Hello, Here is your personal assistant",
       a: "How can I help you?",
     },
-  ]);
+  ];
 
-  const handleSubmit = (event) => {
+  const [query, setQuery] = useState("");
+  const [question, setQuestion] = useState("");
+  const [ans, setAns] = useState("");
+  const [arr, setArr] = useState(a);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
     //Collect the response of the query
-
-    const axios = require("axios");
-
-    const apiKey = "";
-    const client = axios.create({
-      headers: { Authorization: "Bearer " + apiKey },
+    arr.splice(0);
+    const OpenAI = require("openai").OpenAI;
+    const openai = new OpenAI({
+      apiKey: "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",
+      dangerouslyAllowBrowser: true,
     });
-
-    const params = {
-      prompt: query,
-      max_tokens: 200,
-      temperature: 0.07,
-    };
-
-    client
-      .post("https://api.openai.com/v1/engines/davinci/completions", params)
-      .then((result) => {
-        var data = result.data.choices[0].text;
-        console.log(data);
-        console.log(arr);
-        setAns(data);
-      })
-      .catch((err) => {
-        console.log(err);
+    async function main() {
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: query,
+          },
+        ],
       });
+      let data = response.choices[0].message.content;
+      console.log(data);
+
+      setAns(data);
+    }
+    main();
   };
 
-  const addResponse = (query, ans) => {
-    let newData = [
+  const addResponse = async (query, ans) => {
+    await handleSubmit(event);
+    var newData = [
       {
         q: query,
         a: ans,
       },
     ];
-    setArr([...arr, newData]);
+
+    setArr([newData, ...arr]);
     console.log("addResponse done");
+    console.log(arr);
   };
 
   return (
     <div className="container">
       <div>
-        <p>{query}</p>
+        <p>{question}</p>
         <br />
         <p>{ans}</p>
 
-        {arr.map((data, idx) => (
-          <li className="list-none" key={idx}>
-            <p>{data.q}</p>
-            <br />
-            <p>{data.a}</p>
-          </li>
-        ))}
+        {arr.map((data, idx) => {
+          return (
+            <li className="list-none" key={idx}>
+              <p>{data.q}</p>
+              <br />
+              <p>{data.a}</p>
+            </li>
+          );
+        })}
       </div>
       <div className="doubts">
         <input
@@ -78,8 +82,10 @@ export default function doubt() {
         <button
           type="submit"
           onClick={() => {
-            handleSubmit(event);
+            arr.splice(0);
             addResponse(query, ans);
+            setQuestion(query);
+            setQuery("");
           }}
         >
           Enter
