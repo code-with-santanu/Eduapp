3d25037e0a1eeaad8504afb0acda5deb55302d77
pranti <prantibanda@gmail.com>
2023-09-23 06:52:13 +0530

    Description added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,8 +8,8 @@ export default function Home() {
 		<div className="flex flex-col justify-center py-24 px-16 bg-yellow-300 h-screen"> 
 			<div className="flex items-center">
 				<div className=''> {/*-z-5 absolute*/}
-					<div className="text-9xl font-extrabold drop-shadow-lg text-white shadow-black pl-44">GEEKTOPIA</div>
-					<div className="bg-red-100 text-4xl">Description</div>
+					<div className="text-9xl font-extrabold drop-shadow-lg text-white shadow-black pl-44 pb-8">GEEKTOPIA</div>
+					<div className="text-2xl text-blue-900 font-bold text-right px-4 border-r-4  border-blue-900">Tired of switching between various tech platforms? Don't worry! Geektopia is your one stop solution. Starting from organizing your notes, to finding perfect roadmaps and clearing your doubts, we shall provide you everything required for a smooth ride in the tech world. So what are you waiting for? Join us today and become a tech expert with Geektopia!</div>
 				</div>
 				<Image className="w-[30rem] ml-auto" 
 					src={homeimage}
