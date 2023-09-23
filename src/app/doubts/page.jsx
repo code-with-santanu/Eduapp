6ca0742bfe5910819f7c7ce24a52ae80336b2fb4
pranti <prantibanda@gmail.com>
2023-09-23 09:04:37 +0530

    Quiz designing complete

--- src/app/doubts/page.jsx
+++ src/app/doubts/page.jsx
@@ -3,17 +3,18 @@
 import React, { useState } from "react";
 
 export default function doubt() {
-  const a = [
-    {
-      q: "Hello, Here is your personal assistant",
-      a: "How can I help you?",
-    },
-  ];
+  // const a = [
+  //   {
+  //     q: "Hello, Here is your personal assistant",
+  //     a: "How can I help you?",
+  //   },
+  // ];
 
   const [query, setQuery] = useState("");
   const [question, setQuestion] = useState("");
   const [ans, setAns] = useState("");
-  const [arr, setArr] = useState(a);
+  const [show, setShow] = useState(false);
+  const [arr, setArr] = useState([]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -57,39 +58,52 @@ export default function doubt() {
   };
 
   return (
-    <div className="container">
-      <div>
-        <p>{question}</p>
-        <br />
-        <p>{ans}</p>
+    <div className="flex justify-center py-20">
+      <div className="flex flex-col w-2/3 items-center">
+      {(!show) && <div>
+          <div className="text-2xl text-center font-bold bg-white px-6 py-2 rounded-lg text-orange-500 shadow mb-20">Hello, Here is your personal assistant</div>
+          <div className="text-8xl text-center py-10 font-extrabold text-orange-500 drop-shadow-md">GEEKTOPIA</div>
+          <div className="font-semibold text-blue-900 text-2xl text-center ">How can I help you?</div>
+      </div>}
 
-        {arr.map((data, idx) => {
-          return (
-            <li className="list-none" key={idx}>
-              <p>{data.q}</p>
-              <br />
-              <p>{data.a}</p>
-            </li>
-          );
-        })}
-      </div>
-      <div className="doubts">
+      {show && <div className="w-full">
+        <div className="text-2xl font-semibold bg-white px-6 py-3 rounded-lg text-blue-800 shadow mb-5">Q. {question}</div>
+        <div className="flex text-lg px-6 pt-6 pb-12 font-medium bg-white text-slate-700 rounded-lg shadow-md"><div><span className="text-orange-500 bg-orange-100 py-1 px-2 rounded-md">Ans:&nbsp;</span></div><div className="pr-5 pl-3 text-justify">{ans}</div></div>
+        {/* <div className="">
+          {arr.map((data, idx) => {
+            return (
+              <div className="bg-blue-200" key={idx}>
+                <div className=" text-2xl text-center font-semibold bg-white p-2 rounded-lg text-blue-900">{data.q}</div>
+                <br />
+                <div className="text-lg text-center font-semibold">{data.a}</div>
+              </div>
+            );
+          })}
+        </div> */}
+       
+        
+      </div>}
+      <div className="w-full flex justify-center mt-4">
         <input
+        className="px-4 py-2 shadow w-2/5 rounded-full "
           placeholder="Ask your doubt here..."
           value={query}
           onChange={({ target }) => setQuery(target?.value)}
         />
         <button
+          className="bg-white py-2 px-4 rounded-full ml-1 shadow hover:bg-orange-500 hover:text-white font-bold text-slate-700"
           type="submit"
           onClick={() => {
             arr.splice(0);
             addResponse(query, ans);
             setQuestion(query);
             setQuery("");
+            setShow(true);
           }}
         >
-          Enter
+          Submit
         </button>
+      </div>
       </div>
     </div>
   );
